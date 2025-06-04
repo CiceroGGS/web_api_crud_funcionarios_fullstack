acbfc19b73932176a95c67b1288b50f49bc89406
cicero <cicerog.silvestre@gmail.com>
2025-06-04 09:18:05 -0300

    Aterado proj para apsnet webapi

---
+++ Models/FuncionarioModel.cs
@@ -0,0 +1,14 @@
+namespace WebApiFuncionario.Models
+{
+public class FuncionarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Departamento { get; set; }
+        public bool Ativo { get; set; }
+        public string Turno { get; set; }
+        public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataDeAlterecao { get; set; } = DateTime.Now.ToLocalTime();
+    }  
+}
